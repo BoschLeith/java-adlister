d28b897b778d3861a4bf8f0b18b46ddab0ff58d3
Bosch Leith <bosch.leith@gmail.com>
2021-11-29 15:00:43 -0600

    Completed: page view counter servlet

---
+++ src/main/java/ViewCountServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ViewCountServlet", urlPatterns = "/count")
+public class ViewCountServlet extends HttpServlet {
+	int count = 0;
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+
+		count++;
+		out.println("View Count: " + count);
+	}
+}
